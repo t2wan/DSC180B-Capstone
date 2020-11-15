18493c191cdc5524bea09205ead60e65a7166962
maritama <yim095@ucsd.edu>
2020-11-16 03:53:00 +0800

    kk

--- src/auto.py
+++ src/auto.py
@@ -1,19 +1,18 @@
 import subprocess
 import os
 
-def autophrase(input_path):
+def autophrase(output_path):
     mycwd = os.getcwd()
-    if input_path== "data/raw/DBLP.csv":
-        subprocess.run(['brew','install','gcc6'])
-        subprocess.run(['brew','update'])
-        os.chdir('AutoPhrase')
-        subprocess.run(['chmod','+x','auto_phrase.sh'])
-        subprocess.run(['./auto_phrase.sh'])
+    subprocess.run(['brew','install','gcc6'])
+    subprocess.run(['brew','update'])
+    os.chdir('AutoPhrase')
+    subprocess.run(['chmod','+x','auto_phrase.sh'])
+    subprocess.run(['./auto_phrase.sh'])
 
-        os.rename('/AutoPhrase/models/DBLP/AutoPhrase.txt', '/data/outputs/AutoPhrase.txt')
-        os.rename('/AutoPhrase/models/DBLP/AutoPhrase_multi-words.txt', '/data/outputs/AutoPhrase_multi-words.txt')
-        os.rename('/AutoPhrase/models/DBLP/AutoPhrase_single-word.txt', '/data/outputs/AutoPhrase_single-word.txt')
-        os.rename('/AutoPhrase/models/DBLP/token_mapping.txt', '/data/outputs/token_mapping.txt')
-        os.rename('/AutoPhrase/models/DBLP/segmentation.model', '/data/outputs/tsegmentation.model')
-        os.chdir(mycwd)
-        return
+    os.rename('/AutoPhrase/models/DBLP/AutoPhrase.txt', output_path + '/AutoPhrase.txt')
+    os.rename('/AutoPhrase/models/DBLP/AutoPhrase_multi-words.txt', output_path+'/AutoPhrase_multi-words.txt')
+    os.rename('/AutoPhrase/models/DBLP/AutoPhrase_single-word.txt', output_path +'/AutoPhrase_single-word.txt')
+    os.rename('/AutoPhrase/models/DBLP/token_mapping.txt', output_path + '/token_mapping.txt')
+    os.rename('/AutoPhrase/models/DBLP/segmentation.model', output_path +'/segmentation.model')
+    os.chdir(mycwd)
+    return
