189eeba1c8fe7719e714af360ce13f4000547ff4
maritama <yim095@ucsd.edu>
2020-11-16 02:31:11 +0800

    up

--- src/auto.py
+++ src/auto.py
@@ -4,7 +4,7 @@ def autophrase(input_path):
     if input_path== "data/raw/DBLP.csv":
         subprocess.run(['brew','install','gcc6'])
         subprocess.run(['brew','update'])
-        subprocess.run(['cd/AutoPhrase'])
+        subprocess.run(['cd AutoPhrase'])
         subprocess.run(['auto_phrase.sh'])
         subprocess.run(['cd'])
         return
