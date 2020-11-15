629277dad66b48d05d82d832f6b3dbdf8a716d49
maritama <yim095@ucsd.edu>
2020-11-16 04:44:33 +0800

    hh

--- src/auto.py
+++ src/auto.py
@@ -9,10 +9,11 @@ def autophrase(output_path):
     subprocess.run(['chmod','+x','auto_phrase.sh'])
     subprocess.run(['./auto_phrase.sh'])
 
+    os.chdir(mycwd)
     os.rename('/AutoPhrase/models/DBLP/AutoPhrase.txt', output_path + '/AutoPhrase.txt')
     os.rename('/AutoPhrase/models/DBLP/AutoPhrase_multi-words.txt', output_path+'/AutoPhrase_multi-words.txt')
     os.rename('/AutoPhrase/models/DBLP/AutoPhrase_single-word.txt', output_path +'/AutoPhrase_single-word.txt')
     os.rename('/AutoPhrase/models/DBLP/token_mapping.txt', output_path + '/token_mapping.txt')
     os.rename('/AutoPhrase/models/DBLP/segmentation.model', output_path +'/segmentation.model')
-    os.chdir(mycwd)
+
     return
